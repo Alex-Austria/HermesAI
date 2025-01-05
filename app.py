01cce392737069b62d5285114341e3175db237bc
Alexander Kirchmayr <alexanderkirchmayr@MacBook-Pro-2-gen-Chip.local>
2025-01-05 17:58:05 +0100

    Add favicon.ico and update app

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {
-        'message': 'Hello from the API!',
-        'status': 'success'
-    }
-    return jsonify(data)
-
-@app.route('/api/info', methods=['GET'])
-def get_info():
-    info = {
-        'version': '1.0',
-        'author': 'Alex-Austria'
-    }
-    return jsonify(info)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
